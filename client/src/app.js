ffc1f2436f6bbd7078a4eb6b2a9c16b3ab6dd015
Elgrogz <gregorgilchrist@gmail.com>
2017-02-19 15:27:30 +0000

    basic main container set up

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,11 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
+import MainContainer from './containers/MainContainer.jsx';
+
 
 window.onload = function(){
   ReactDOM.render(
-    <h1> App Started </h1>,
+    <MainContainer />,
     document.getElementById('app')
   );
 }
---
+++ client/src/containers/MainContainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// import Tile from './Tile.jsx';
+
+class MainContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return (
+        <table className= "Table">
+        <tbody>
+          <tr>
+            hello
+          </tr>
+          <tr>
+            hello
+          </tr>
+          <tr>
+            hello
+          </tr>
+        </tbody>
+      </table>
+      )
+    }
+
+}
+
+module.exports = MainContainer;
